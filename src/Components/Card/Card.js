bf8d1cabfecdf6172ac3130c8a42595eb334ffa9
Thetruevinny <abhik.vinod14@gmail.com>
2024-09-13 11:45:20 +0100

    Final Touch up for Static Features

--- src/Components/Card/Card.js
+++ src/Components/Card/Card.js
@@ -1,21 +1,20 @@
 import React from "react";
 import upVote from '../../imgs/Votes/upVote.png';
-import downVote from '../../imgs/Votes/downVote.png';
 import commentsImg from '../../imgs/Comments.png';
 import Styles from '../../Styles/Card.module.css';
 
 function Card({ info }) {
     const {votes, img, description, author, time, comments} = info;
-    
+
     return(
         <div name='comment' className={Styles.comment}>
             <div name='votes' className={Styles.votes}>
                 <img src={upVote} alt="Up arrow" />
                 <p>{votes}</p>
-                <img src={downVote} alt="Down arrow" />
+                <img src={upVote} alt="Down arrow" className={Styles.up}/>
             </div>
             <div name='Content' className={Styles.content}>
-                <img src={img} alt='Flame' />
+                <img src={img} alt='Flame'  />
                 <p>{description}</p>
                 <hr></hr>
                 <div name='user interactions' className={Styles.info}>
